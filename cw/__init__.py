4fe1366260610c7dd8e0a6f5c807c6863312b8fd
vjache <vjache@gmail.com>
2025-04-04 17:15:57 +0300

    Configure __init__.py

---
+++ cw/__init__.py
@@ -0,0 +1,6 @@
+from .cw import CellWorld, Cell, Agent, Item, ItemType, Direction
+from .cwviz import CellWorldVisualizer
+
+__version__ = "0.0.1"
+__author__ = "Vyacheslav vjache@gmail.com"
+__email__ = "vjache@gmail.com"
